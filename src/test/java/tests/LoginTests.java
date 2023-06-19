a5bf19d3dc2d94d532bf94325227df3cfaf3f065
MariaZH13 <134274880+MariaZH13@users.noreply.github.com>
2023-06-19 14:04:23 +0300

    refactorLoginPositive19.06

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -40,33 +40,24 @@ public class LoginTests extends TestBase {
 
       }
 
-    //    @Test
-//    public void loginPositiveTest(){
-//        // open login form
-//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-//
-//
-//        // fill login form
-//       WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-//       emailInput.click();
-//       emailInput.clear();
-//       emailInput.sendKeys("marzh@com");
-//
-//       WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-//       passInput.click();
-//       passInput.clear();
-//       passInput.sendKeys("Qwe1234$");
-//
-//        // click on button Login
-//        wd.findElement(By.xpath("//button[1]")).click();
-//
-//        // Assert
-//        // Assert.assertTrue(wd.findElements(By.xpath("//*[.='Sign Out']")).size() >= 0);
-//        pause(3000);
-//        Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
-//
-//
-//    }
+        @Test
+    public void loginPositiveTest(){
+        String email="marzh@com",password="Qwe1234$";
+        // open login form
+        app.getUser().openLoginForm();
+
+        // fill login form
+       app.getUser().fillLoginForm(email,password);
+
+        // click on button Login
+        app.getUser().submitLogin();
+
+        // Assert
+        app.getUser().pause(3000);
+        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//button")));
+
+
+    }
 
  //   @Test
  //   public void loginNegativeTestWrongEmail(){
